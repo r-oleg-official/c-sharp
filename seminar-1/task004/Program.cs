45d4449c119aaa215cfcdcc489447643bcbf52d1
r-oleg-official <r.oleg.official@gmail.com>
2022-06-28 18:50:10 +0300

    Add 'seminar-1' in 'c-sharp'.

---
+++ seminar-1/task004/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Input a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max;
+if (b < a && b > c) {
+    max = b;
+}
+if (a < b && b > c) {
+    max = b;
+}
+else {
+    max = c;
+}
+Console.WriteLine("Maximum number: " + max);
