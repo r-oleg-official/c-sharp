f1f65e2ae57afeffe508e4a63722675c1ba7bfed
r-oleg-official <r.oleg.official@gmail.com>
2022-06-28 18:49:21 +0300

    'lecture-1 and 'C#_manual.md'.

--- lecture-1/Example003_Sum/Program.cs
+++ lecture-1/Example003_Sum/Program.cs
@@ -2,7 +2,7 @@
 //int numberB = 5;
 //Console.WriteLine(numberA + numberB);
 
-//дополнительная переменная с результатом
+//дополнительная переменная с результатом.
 int numberA = 3;
 int numberB = 5;
 int result = numberA + numberB;
