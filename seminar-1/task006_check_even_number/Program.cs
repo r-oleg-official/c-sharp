905ef22d1bb0b47ce5ab9e9bb5f632d7d8f4cac0
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-01 23:42:20 +0300

    Done "task006_check_even_number".

---
+++ seminar-1/task006_check_even_number/Program.cs
@@ -0,0 +1,10 @@
+﻿// Input number. Check is even number?
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.Read());
+
+if (number%2 == 0) {
+    Console.WriteLine(" Чётное.");
+}
+else {
+    Console.WriteLine(" Нечётное.");
+}
