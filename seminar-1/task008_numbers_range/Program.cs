846bdeae2b2206ceedd1d24b9163ff5fb9c84f56
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-02 00:29:51 +0300

    Edit "task008_numbers_range".

---
+++ seminar-1/task008_numbers_range/Program.cs
@@ -0,0 +1,15 @@
+﻿// Input number: N. See all even numbers from 1 to N.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToUInt16(Console.Read());
+int count = 2;
+Console.Write(number);
+//while (count < number) {
+//       Console.Write(count);
+//        if (count == number -1) {
+//            Console.Write(".");
+//        }
+//        else {
+//        Console.Write(", ");
+//        }
+//    count = count +2;    
+//}
