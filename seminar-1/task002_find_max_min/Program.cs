80f97095a30111df2b62f253b87bd981b1e1c067
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-01 23:03:06 +0300

    Done task002_find_max_min_number.

---
+++ seminar-1/task002_find_max_min/Program.cs
@@ -0,0 +1,18 @@
+﻿// Input two numbers, find max'n'min number.
+int max_number = 0, min_number = 0;
+
+Console.Write("Введите первое число ");
+int first_number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int second_number = Convert.ToInt32(Console.ReadLine());
+
+if (first_number > second_number) {
+    max_number = first_number;
+    min_number = second_number;
+}
+else {
+    min_number = first_number;
+    max_number = second_number;
+}
+Console.WriteLine("Максимальное число: " + max_number);
+Console.WriteLine("Минимальное число: " + min_number);
