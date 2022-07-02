50bf6e8d8107d2d6cb139eaa2a566d1bfc86a84d
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-02 10:50:12 +0300

    Example001_x_y_quarter

---
+++ seminar-3/Example001_x_y_quarter/Program.cs
@@ -0,0 +1,38 @@
+﻿while(true)
+{
+    Console.Clear();
+    int x = InputInt("Введите X: ");
+    int y = InputInt("Введите Y: ");
+
+    if(x == 0 || y == 0)
+    {
+        Console.WriteLine("X и Y не должны быть равны 0");
+        Console.ReadKey();
+        continue;
+    }
+
+    if(x > 0 && y > 0)
+    {
+        Console.WriteLine("1 четверть");
+    }
+    else if(x < 0 && y > 0)
+    {
+        Console.WriteLine("2 четверть");
+    }
+    else if(x < 0 && y < 0)
+    {
+        Console.WriteLine("3 четверть");
+    }
+    else if(x > 0 && y < 0)
+    {
+        Console.WriteLine("4 четверть");
+    }
+
+    break;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
