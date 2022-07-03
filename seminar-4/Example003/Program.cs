30f42191eddedb2b08267cc6da6cede3442e8a1d
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-03 17:39:31 +0300

    Add "seminar-4/Example003".

---
+++ seminar-4/Example003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Factorial of number.
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int factorial = 1;
+
+for (int count = 1; count <= number; count++)
+{
+    factorial = factorial * count;
+}
+Console.WriteLine("Факториал числа " + number + ": " + factorial + ".");
