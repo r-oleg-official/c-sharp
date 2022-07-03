cad3f13717663875f11d320cd321c8b51a8ad4b9
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-03 21:26:16 +0300

    Add "seminar-1/Example001_quadro".

---
+++ seminar-1/Example001_quadro/Program.cs
@@ -0,0 +1,5 @@
+﻿// Quadro number.
+Console.Write("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int quadro = a * a;
+Console.WriteLine("Квадрат числа " + a + ": " + quadro + ".");
