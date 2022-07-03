0f15ae82ce8ad9d0ae4145c9daa98902784d6c3b
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-03 10:12:20 +0300

    Add seminar-4/Example001/.

---
+++ seminar-4/Example001/Program.cs
@@ -0,0 +1,17 @@
+﻿//Найти сумму ряда чисел до A
+Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int count = 1;
+while (count <= a)
+{
+    sum += count; // sum = sum + count
+    count++;
+}
+Console.WriteLine(sum);
+
+for (int i = 1; i <= a; i++)
+{
+    sum += i;
+}
+Console.WriteLine(sum);
