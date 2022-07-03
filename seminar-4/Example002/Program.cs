20647f06c61b0883f121ceec4fe80d64ab3e527b
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-03 17:36:13 +0300

    Add "seminar-4/Example002".

---
+++ seminar-4/Example002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Определение кол-ва разрядов числа.
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Количество разрядов числа " + num + ": ");
+
+int count =0;
+while (num > 0)
+{
+    num = num / 10;
+    count++;
+}
+Console.WriteLine(count + ".");
