e0e81162230d290156b3718f520e289d38788f50
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-03 17:47:08 +0300

    Add "seminar-4/Example005_Denis_array".

---
+++ seminar-4/Example005_Denis_array/Program.cs
@@ -0,0 +1,17 @@
+﻿// 
+int[] array = new int[8];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 2);
+}
+PrintArray(array);
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }   
+    Console.WriteLine();
+}
