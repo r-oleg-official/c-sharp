f2384d534575ded89aa4b4104291a8313ac2ba33
r-oleg-official <r.oleg.official@gmail.com>
2022-07-04 10:04:18 +0300

    Add my version "seminar-1/Example002_check_quadro-1".

---
+++ seminar-1/Example002_check_quadro-1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Enter two numbers and check if they are squares to each other.
+//My version.
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 * number1 == number2)
+{
+    Console.WriteLine("Второе число квадрат первого.");
+}
+else if (number2 * number2 == number1)
+{
+    Console.WriteLine("Первое число квадрат второго.");
+}
+else
+{
+    Console.WriteLine("Ни одно из чисел не является квадратом другого.");
+}
