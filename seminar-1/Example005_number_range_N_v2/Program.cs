c1a9d2fc2b23a351f63f64c4fafd44617ba0c66e
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-04 20:25:44 +0300

    Add "seminar-1/Example005_number_range_N_v2".

---
+++ seminar-1/Example005_number_range_N_v2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Input number N. Output to console range of the numbers from -N to N.
+//Version by Sasha.
+int a = 5;
+int b = -a;
+string c = "";
+while (b!=a)
+{
+    c = c + ", " + b++;
+}
+Console.WriteLine(c);
+// Output: , -5, -4, -3, -2, -1, 0, 1, 2, 3, 4
