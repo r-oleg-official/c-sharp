1337d96e86a6a7eb675fa5eeca5b781cbed08029
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-04 20:57:49 +0300

    Add version by Sasha, but dont work "seminar-1/Example007_last_digit_v2".

---
+++ seminar-1/Example007_last_digit_v2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Entering a three-digit number. Displaying the last digit.
+//My version.
+Console.WriteLine("Введите любое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (1000 < a <= 100)
+//Error: оператор "<=" нельзя применить к операнду типа "bool", или "int".
+{
+    int d = a % 10;
+    Console.WriteLine("Последний знак числа: " + d);
+}
+else Console.WriteLine("Это не трёхзначное число!!!");
