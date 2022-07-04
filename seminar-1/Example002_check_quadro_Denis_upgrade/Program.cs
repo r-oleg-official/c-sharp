c3a70f68809afe67ce885c94bd5c568f403bb63f
r-oleg-official <r.oleg.official@gmail.com>
2022-07-04 11:43:49 +0300

    Add "seminar-1/Example002_check_quadro_Denis_upgrade".

---
+++ seminar-1/Example002_check_quadro_Denis_upgrade/Program.cs
@@ -0,0 +1,14 @@
+﻿// Upgrade code by Denis.
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a * a == b || b * b == a)
+{
+    Console.WriteLine("ДА");
+}
+else if (a * a != b || b * b != a)
+{
+    Console.WriteLine("НЕТ");
+}
