84a65bd4e3935e8e730602fc1b82ae7d6f7f9cd3
r-oleg-official <r.oleg.official@gmail.com>
2022-07-04 10:33:15 +0300

    Add my version "seminar-1/Example003_days_week".

---
+++ seminar-1/Example003_days_week/Program.cs
@@ -0,0 +1,37 @@
+﻿// Ввод: день недели по номеру. Вывод: название дня недели.
+// My version.
+Console.Write("Введите день недели числом от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1)
+{
+    Console.WriteLine("Понедельник.");
+}
+if (day == 2)
+{
+    Console.WriteLine("Вторник.");
+}
+if (day == 3)
+{
+    Console.WriteLine("Среда.");
+}
+if (day == 4)
+{
+    Console.WriteLine("Четверг.");
+}
+if (day == 5)
+{
+    Console.WriteLine("Пятница.");
+}
+if (day == 6)
+{
+    Console.WriteLine("Суббота.");
+}
+if (day == 7)
+{
+    Console.WriteLine("Воскресенье.");
+}
+if (day < 1 | day > 7)
+{
+    Console.WriteLine("Введен неверный номер дня недели. Введите от 1 до 7.");
+}
