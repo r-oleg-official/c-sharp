2d575562494dc830cd9f62f87534f9783981bef7
r-oleg-official <r.oleg.official@gmail.com>
2022-07-04 12:28:16 +0300

    Add my version "seminar-1/Example005_number_range_N".

---
+++ seminar-1/Example005_number_range_N/Program.cs
@@ -0,0 +1,15 @@
+﻿//Input number N. Output to console range of the numbers from -N to N.
+//My version.
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+//int minN = -n;
+
+for (int i = -n; i < (n + 1); i++)
+{
+    if (i == n)
+    {
+        Console.WriteLine(i + ".");
+    }
+    else
+    Console.Write(i + ", ");
+}
