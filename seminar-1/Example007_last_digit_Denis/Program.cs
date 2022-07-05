cb7e3cebea714e49967d680f962c6580c1bfd019
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-05 23:17:55 +0300

    Add home version "seminar-1/Example007_last_digit_Denis".

---
+++ seminar-1/Example007_last_digit_Denis/Program.cs
@@ -0,0 +1,12 @@
+﻿//Entering a three-digit number. Displaying the last digit.
+// Version by Denis.
+Console.Write("Введите трёхзначое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99)
+{
+    if (number < 1000)
+    {
+        int result = number % 10;
+        Console.WriteLine(result);
+    }
+}
