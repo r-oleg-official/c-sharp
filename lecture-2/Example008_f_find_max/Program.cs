dbba566e6be07fd71f8afa3f1fa6c34903169ec4
r-oleg-official <r.oleg.official@gmail.com>
2022-07-05 17:00:06 +0300

    Add "lecture-2/Example008_f_find_max".

---
+++ lecture-2/Example008_f_find_max/Program.cs
@@ -0,0 +1,28 @@
+﻿//Find maximum number from a 9 numbers.
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+
+int a2 = 11;
+int b2 = 12;
+int c2 = 18;
+
+int a3 = 55;
+int b3 = 78;
+int c3 = 99;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max);
