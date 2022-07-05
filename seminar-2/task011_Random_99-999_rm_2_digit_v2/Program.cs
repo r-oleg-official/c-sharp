1776a1a9ea9a60f358eb947097acf4df6b46f072
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-05 23:51:46 +0300

    Add "seminar-2/task011_Random_99-999_rm_2_digit_v2".

---
+++ seminar-2/task011_Random_99-999_rm_2_digit_v2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Displaying a random number 99-999, remove two digit of the number. Displaying the result.
+// My version 2.0.
+int randomNumber = new Random().Next(100, 1000);
+int digit1 = randomNumber / 100;
+int digit2 = randomNumber % 10;
+int result = digit1 * 10 + digit2;
+Console.WriteLine("Cлучайное число: " + randomNumber);
+Console.WriteLine("Результат: " +result);
