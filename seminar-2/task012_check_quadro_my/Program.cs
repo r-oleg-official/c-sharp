edfd2ce0c7751939078d52db54dee6eeabee9e61
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-06 00:18:50 +0300

    Add "seminar-2/task012_check_quadro_my".

---
+++ seminar-2/task012_check_quadro_my/Program.cs
@@ -0,0 +1,22 @@
+﻿//Enter two numbers and check if they are squares to each other.
+//If first number don't quadro the second number to displaying 
+//the remainder to the division.
+//My version.
+Console.Write("Первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Кратно.");
+}
+else if (number2 == 0)
+{
+    Console.WriteLine("Нельзя делить на нуль!");
+}
+else if (number1 != number2 * number2)
+{
+    int remain = number1 % number2;
+    Console.WriteLine("Не кратно, " + remain);
+}
