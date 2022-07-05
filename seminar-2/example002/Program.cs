d9ca98dfff38bcea51cd5a7e5f6716aec5bae5c7
r-oleg-official <r.oleg.official@gmail.com>
2022-07-05 13:11:22 +0300

    Add my version "seminar-2/task011_Random_99-999_rm_2_digit".

--- seminar-2/example002/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿int number = 105;
-
-if (number > 9 || number < 100)
-{
-    Console.WriteLine(number);
-}
---
+++ seminar-2/task011_Random_99-999_rm_2_digit/Program.cs
@@ -0,0 +1,6 @@
+﻿// Displaying a random number 99-999, remove two digit of the number. Displaying the result.
+// My version.
+int randomNumber = new Random().Next(100, 1000);
+string digit2 = $"{randomNumber / 100}{randomNumber % 10}";
+int result = Convert.ToInt32(digit2);
+Console.WriteLine($"Случайное число = {randomNumber}, результат = {digit2}");
