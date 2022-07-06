d7d8f57d54c84f3d8a264fe56955ad53493af303
r-oleg-official <r.oleg.official@gmail.com>
2022-07-06 12:44:22 +0300

    Add "seminar-4/task025_number_degree_n_dont_work". Dont work!

---
+++ seminar-4/task025_number_degree_n_dont_work/Program.cs
@@ -0,0 +1,16 @@
+﻿// Entering a number  and displaying in degree n.
+// Version by mine.
+//Don't work!
+Console.Write("Введи число от 1 и более: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи степень: ");
+int degreeNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write(number);
+int index = 1;
+//int result = number;
+while (index < degreeNumber)
+{
+    number = number * number;
+    index++;
+}
+Console.WriteLine($"^{degreeNumber} -> {number}.");
