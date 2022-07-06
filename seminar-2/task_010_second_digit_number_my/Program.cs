94223d8b797d7f631e0139a3b18b2a71f51a9dfc
r-oleg-official <r.oleg.official@gmail.com>
2022-07-06 10:14:23 +0300

    Add "seminar-2/task_010_second_digit_number_my".

---
+++ seminar-2/task_010_second_digit_number_my/Program.cs
@@ -0,0 +1,15 @@
+﻿// Enter a three-digit number and display the second digit of the number.
+// My version.
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit2 = number % 100;
+
+if (number > 99 & number < 1000)
+{
+    digit2 = digit2 / 10;
+    Console.WriteLine($"Вторая цифра числа: {digit2}.");
+}
+else
+{
+     Console.WriteLine("Неверно! Нужно вводить трёхзначное число.");
+}
