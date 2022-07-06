d8d49da5e14764a6e2f29778bcf27d7c89f6f0c7
r-oleg-official <r.oleg.official@gmail.com>
2022-07-06 12:36:33 +0300

    Add "seminar-4/task025_number_degree_n".

---
+++ seminar-4/task025_number_degree_n/Program.cs
@@ -0,0 +1,15 @@
+﻿// Entering a number  and displaying in degree n.
+// Version by mine.
+Console.Write("Введи число от 1 и более: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи степень: ");
+int degreeNumber = Convert.ToInt32(Console.ReadLine());
+
+int index = 1;
+int result = number;
+while (index < degreeNumber)
+{
+    result = result * number;
+    index++;
+}
+Console.WriteLine($"{number}^{degreeNumber} -> {result}.");
