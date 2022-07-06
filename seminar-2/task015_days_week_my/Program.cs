75340f8faf56a8807c61543dcc4ce15e4502581c
r-oleg-official <r.oleg.official@gmail.com>
2022-07-06 11:48:54 +0300

    Add "seminar-2/task015_days_week_my".

---
+++ seminar-2/task015_days_week_my/Program.cs
@@ -0,0 +1,17 @@
+﻿// Enter number a day of the week and displaying only a weekend? Yes, or no.
+// Version by mine.
+Console.Write("Введите день недели цифрой от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 ^ number > 7)
+{
+    Console.WriteLine("Неверно! Нужно вводить от 1-7.");
+}
+else if (number > 5 & number < 8)
+{
+    Console.WriteLine(" -> да.");
+}
+else
+{
+    Console.WriteLine(" -> нет.");
+}
