3de3983d49ad9e0af89e27314b4a814d2bd6d28b
r-oleg-official <r.oleg.official@gmail.com>
2022-07-06 10:41:55 +0300

    Add "seminar-2/task013_display_digit3_my".

--- seminar-2/task013_display_digit3_my/Program.cs
+++ seminar-2/task013_display_digit3_my/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// My version.
+int a = 645;
+int b = 78;
+int c = 32679;
+
+Console.Write(a);
+a = a % 10;
+Console.WriteLine($" -> {a}");
+
+Console.WriteLine($"{b} -> третьей цифры нет");
+
+Console.Write(c);
+c = c / 100;
+c = c % 10;
+Console.WriteLine($" -> {c}");
