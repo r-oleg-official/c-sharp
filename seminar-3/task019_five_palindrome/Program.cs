ae2a8dc9765912ea2c3b8d1c9e1cb5c461f0acd9
r-oleg-official <r.oleg.official@gmail.com>
2022-07-08 10:27:48 +0300

    Well Done a simple version is finding the palindrome to "seminar-3/task019_five_palindrome".

--- seminar-3/task019_five_palindrome/Program.cs
+++ seminar-3/task019_five_palindrome/Program.cs
@@ -1,5 +1,4 @@
-﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-// Entering a five-digit number and check it's a palindrome?
+﻿// Entering a five-digit number and check it's a palindrome?
 // My simple version.
 
 Console.Write("Enter a five-digit number: ");
@@ -11,58 +10,28 @@ if (number < 10000 && number > 9999)
 }
 else if ( number > 9999 && number < 100000)
     {
-        //Stage-1: get first and last digit of the number
+        // Stage-1: get first and last digit of the number
         int count = 10;
         int max_count = 10000;
         int digit1 = number / max_count;
         int digit2 = number % count;
-        //Stage-2: cut begin and end of number.
+        
+        // Stage-2: cut begin and end of number.
         number = number % max_count;
         number = number / count;
         max_count = 100;
+        
+        // Stage-3: get first and last digit of the number
         int digit3 = number / max_count;
         int digit4 = number % count;
+        
+        // Stage-4: digits are matching?
         if (digit1 == digit2 && digit3 == digit4)
         {
-            Console.WriteLine($"{digit1}, {digit2}");
+            Console.WriteLine("It's palindrome!");
         }
-        Console.WriteLine($"{digit1}, {digit2}");
-        Console.WriteLine($"{digit3}, {digit4}");
+        else
+        {
+            Console.WriteLine("It's not palindrome!");
+        }
     }
-
-
-
-
-
-
-
-//        int count = 10;
-//        int max_count = 10000;
-//        boolean tumbler = true;
-//        while (tumbler == true)
-//
-//        int digit1 = number / max_count;
-//        int digit2 = number % count;
-//        if (digit1 == digit2) tumbler = true;
-//        if (digit1 != digit2) tumbler = false;
-//
-//        max_count = max_count / 100;
-//
-//
-
-
-
-//ВозвращаемыйТипДанных ИмяМетода([ТипДанных1 ИмяАргумента1, ...]) <br>
-//
-//    { 
-//        Тело Метода 
-//        return ЗначениеСоответствующееВозвращаемомуТипуДанных;
-//    }
-//
-//
-// double f(double x)
-//    {
-//        double result = x * x + 1;
-//        return result
-//    }
-	
