3ac8454d9d34512fb33cbb44f76d5f03ffdbaa3e
r-oleg-official <r.oleg.official@gmail.com>
2022-07-08 11:14:16 +0300

    Add version with methods. "seminar-3/task019_five_palindrome_method".

--- seminar-3/task019_five_palindrome_method/Program.cs
+++ seminar-3/task019_five_palindrome_method/Program.cs
@@ -1,47 +1,55 @@
-﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-// Entering a five-digit number and check it's a palindrome?
+﻿// Entering a five-digit number and check - it's a palindrome?
 
-Console.Write("Введи пятизначное число: ");
+Console.Write("Enter a five-digit number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int count = 10;
-int max_count = 10000;
-bool tumbler1 = Palindrome(number, max_count, count);
-
-Console.WriteLine($"{number}, {max_count}, {count}");
-Console.WriteLine(tumbler1);
-
-max_count = 100;
-number = CutNumber(number, max_count, count);
-Console.WriteLine($"{number}, {max_count}, {count}");
-
-bool tumbler2 = Palindrome(number, max_count, count);
-Console.WriteLine(tumbler2);
-if (tumbler1 == tumbler2)
+if (number > 9999 && number < 100000)
 {
-    Console.WriteLine("It's palindrome!");
-}
-else
-{
-    Console.WriteLine("It's not palindrome!");
-}
-// Функции:
-bool Palindrome(int number, int max_count, int count)
-{
-    int digit1 = number / max_count;
-    int digit2 = number % count;
-    if (digit1 == digit2) 
-        {
-            return true;
-        }
+    int count = 10;
+    int max_count = 10000;
+    int cutter = 100; // scissors for extreme numbers.
+
+    // Stage-1: check first and last digits is matching?
+    bool tumbler1 = Palindrome(number, max_count, count);
+
+    // Stage-2: cut first and last digits.
+    number = CutNumber(number, max_count, count);
+    max_count = max_count / cutter;
+
+    // Stage-3: check first and last digits is matching?
+    bool tumbler2 = Palindrome(number, max_count, count);
+    
+    // Output result
+    if (tumbler1 == true && tumbler2 == true)
+    {
+        Console.WriteLine("It's palindrome!");
+    }
     else
-        {
-            return false;
-        }
+    {
+        Console.WriteLine("It's not palindrome!");
+    }
+    // Functions:
+    bool Palindrome(int number, int max_count, int count)
+    {
+        int digit1 = number / max_count;
+        int digit2 = number % count;
+        if (digit1 == digit2) 
+            {
+                return true;
+            }
+        else
+            {
+                return false;
+            }
+    }
+    int CutNumber(int number, int max_count, int count)
+    {
+        number = number % max_count;
+        number = number / count;
+        return number;
+    }
 }
-int CutNumber(int number, int max_count, int count)
+else 
 {
-    number = number % max_count;
-    number = number / count;
-    return number;
+    Console.WriteLine("Error! Need entering a five-digit number");
 }
