8d9f39f96962a5dd08b09344aa4956571efdf8e1
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-08 23:25:32 +0300

    Add "seminar-3/task023_table_cubes_1_N".

---
+++ seminar-3/task023_table_cubes_1_N/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// Entering a number and displaying table of the numbers in cubes.
+Console.Write("Enter a number N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 )
+{
+    Console.WriteLine("Error! Need enter a number from one and high.");
+}
+else
+{
+    Console.Write($"{number} -> ");
+    for (int i =1; i < number; i++)
+    {
+        int result = i * i * i;
+        Console.Write($"{result}, ");
+    }
+    number = number * number * number;
+    Console.WriteLine($"{number}.");
+}
