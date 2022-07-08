5a537296de3d09af0611b3797dad1277448a8386
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-08 22:57:26 +0300

    Well Done "seminar-3/task021_segment_length_3D".

--- seminar-3/task021_segment_length_3D/Program.cs
+++ seminar-3/task021_segment_length_3D/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Entering a two coordinates and calculating length of the segment between to the coordinates. 3D.
 // My version.
-
+string x = "x", y = "y", z = "z";
 Console.WriteLine("Enter coordinates a first point: ");
 int x1 = InputCoordinate(x, 1);
 int y1 = InputCoordinate(y, 1);
@@ -10,26 +10,18 @@ int x2 = InputCoordinate(x, 2);
 int y2 = InputCoordinate(y, 2);
 int z2 = InputCoordinate(z, 2);
 
-// Find AB. CB=x1-x2. BC=x2-x1. AC=y1-y2.
-//double cb = x1 - x2;
-//double ac = y1 - y2;
-//double ab = Math.Sqrt((ac * ac + cb * cb));
-
-// Find AB. CB=x1-x2. BC=x2-x1. AC=y1-y2.
+// Find AB. AB^2 = (xb -xa)^2 + (yb -ya)^2 + (zb -za)^2.
+double xx = x2 - x1;
+double yy = y2 - y1;
+double zz = z2 - z1;
+double ab = Math.Sqrt((xx * xx + yy * yy + zz * zz));
 
 Console.WriteLine($"Length of segment AB={Math.Round(ab, 2)}.");
 
 // Methods:
 int InputCoordinate(string point, int i)
     {
-        Console.Write($"{point}{i} = : ");
+        Console.Write($"{point}{i}= ");
         int x = Convert.ToInt32(Console.ReadLine());
         return x;
     }
-
-//int InputY(int i)
-//    {
-//        Console.Write($"y{i} = : ");
-//        int y = Convert.ToInt32(Console.ReadLine());
-//        return y;
-//    }
