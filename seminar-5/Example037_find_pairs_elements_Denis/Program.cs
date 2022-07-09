2529c3831d1d90f671685604e7e4b01351529c93
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-10 00:32:47 +0300

    Add "seminar-5/Example037_find_pairs_elements_Denis".

---
+++ seminar-5/Example037_find_pairs_elements_Denis/Program.cs
@@ -0,0 +1,44 @@
+﻿int size = InputInt("Введите размер: ");
+int[] numbers = new int[size];
+int[] multiplyPairs;
+FillArray(numbers);
+PrintArray(numbers);
+if(numbers.Length % 2 == 0)
+{
+  multiplyPairs = new int[numbers.Length / 2];
+}
+else
+{
+  multiplyPairs = new int[numbers.Length / 2 + 1];
+  multiplyPairs[multiplyPairs.Length - 1] = numbers[numbers.Length / 2];
+}
+
+for(int i = 0; i < numbers.Length / 2; i++)
+{
+  multiplyPairs[i] = numbers[i] * numbers[numbers.Length - 1 - i];
+}
+PrintArray(multiplyPairs);
+
+
+int InputInt(string output)
+{
+  Console.Write(output);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int[] array)
+{
+  for(int i = 0; i< array.Length; i++)
+  {
+    array[i] = new Random().Next(1, 124);
+  }
+}
+
+void PrintArray(int[] array)
+{
+  for(int i = 0; i< array.Length; i++)
+  {
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
