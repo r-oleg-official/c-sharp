66ffaca864b91f96d85371830eeb1878a8c97149
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-09 22:20:24 +0300

    Add "lecture-3/Example012_Methods". Need  edit.

---
+++ lecture-3/Example012_Methods/Program.cs
@@ -0,0 +1,39 @@
+﻿// View group methods 1.
+void Method1()
+{
+    Console.WriteLine("Hello, World!");
+}
+Method1();
+
+// View group methods 2.
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Text message");
+
+void Method3(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Бывают именованные аргументы. Когда нужно какому именно аргументу какое 
+// значение необходимо указать.
+Method3(msg: "Name message");
+
+void Method21(string msg, int count)
+{
+    int i =0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);    
+        i++;
+    }
+    
+}
+Method21("Text", 4);
+// or
+Method21(msg: "Text2", count: 4);
+Method21(count: 4, msg: "Text3");
+
+// View group methods 3.
+
