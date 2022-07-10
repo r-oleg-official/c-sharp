287e217425a8e541bdc18b45ebe4760834881a73
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-10 15:01:17 +0300

    Add "seminar-6/Example039_flip_array".

---
+++ seminar-6/Example039_flip_array/Program.cs
@@ -0,0 +1,36 @@
+﻿int size = InputInt("Введите размер: ");
+int[] numbers = new int[size];
+FillArray(numbers);
+PrintArray(numbers);
+
+for(int i = 0; i < numbers.Length / 2; i++)
+{
+  int temp = numbers[i];
+  numbers[i] = numbers[numbers.Length - 1 - i];
+  numbers[numbers.Length - 1 - i] = temp;
+}
+
+PrintArray(numbers);
+
+int InputInt(string output)
+{
+  Console.Write(output);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int[] array)
+{
+  for(int i = 0; i< array.Length; i++)
+  {
+    array[i] = new Random().Next(1, 10);
+  }
+}
+
+void PrintArray(int[] array)
+{
+  for(int i = 0; i< array.Length; i++)
+  {
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
