8eaed0fa4523f22d810e0ce5b6478b6d8fd7e3f6
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-10 15:18:42 +0300

    Add "seminar-6/Example040_check_delta".

---
+++ seminar-6/Example040_check_delta/Program.cs
@@ -0,0 +1,24 @@
+﻿// Entering three numbers and check is a delta?
+// My version.
+
+int sideA = InputNumber("Enter one number: ");
+int sideB = InputNumber("Enter two number: ");
+int sideC = InputNumber("Enter three number: ");
+
+if (sideA < sideB + sideC &&
+    sideB < sideA + sideC &&
+    sideC < sideA + sideB)
+{
+    Console.WriteLine("This is delta!");
+}
+else 
+{
+    Console.WriteLine("This is not delta!");
+}
+
+int InputNumber(string msg)
+{
+    Console.Write(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
