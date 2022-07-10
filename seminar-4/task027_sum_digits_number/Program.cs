40378f2149a0f91a932da5239f575ccfac4867f8
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-10 18:24:58 +0300

    Add "seminar-4/task027_sum_digits_number".

---
+++ seminar-4/task027_sum_digits_number/Program.cs
@@ -0,0 +1,17 @@
+﻿// Entering number and displaying a sum digits of the number.
+int number = InputNumber("Enter a number: ");
+
+int result = 0;
+while (number > 0)
+{
+    result = result + number % 10;
+    number = number / 10;
+}
+
+Console.WriteLine($" -> {result}.");
+
+int InputNumber(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
