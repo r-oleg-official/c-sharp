53d553cfa8136b549660204f2214ecacf4351ff0
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-10 17:40:49 +0300

    Add "seminar-6/Example044_Fibonachchi_string_Denis".

---
+++ seminar-6/Example044_Fibonachchi_string_Denis/Program.cs
@@ -0,0 +1,27 @@
+﻿// Entering number N and displaying numbers of a Fibonachchi.
+// First and second numbers of the Fibonachchi: 0 and 1.
+// Version Denis.
+int count = InputInt("Введите, сколько чисел вы хотите вывести: ");
+string result = string.Empty;
+int first = 0;
+int second = 1;
+
+if(count > 0)
+  result += first + " ";
+if(count > 1)
+  result += second + " ";
+
+for(int i = 2; i < count; i++)
+{
+  int next = first + second;
+  result += next + " " ;
+  first = second;
+  second = next;
+}
+Console.WriteLine(result);
+
+int InputInt(string str)
+{
+  Console.Write(str);
+  return Convert.ToInt32(Console.ReadLine());
+}
