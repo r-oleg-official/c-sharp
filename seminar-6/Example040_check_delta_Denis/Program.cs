b82b35bc47b32398b607960cd653f181b0230f66
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-10 15:23:51 +0300

    Add "seminar-6/Example040_check_delta_Denis".

---
+++ seminar-6/Example040_check_delta_Denis/Program.cs
@@ -0,0 +1,21 @@
+﻿int firstSide = InputInt("Введите длину сторону А: ");
+int secondSide = InputInt("Введите длину сторону B: ");
+int thirdSecond = InputInt("Введите длину сторону С: ");
+
+if(firstSide < secondSide + thirdSecond && 
+   secondSide < firstSide + thirdSecond && 
+   thirdSecond < secondSide + firstSide )
+{
+    Console.WriteLine("Существует");
+}
+else
+{
+    Console.WriteLine("Не существует");
+}
+
+
+int InputInt(string output)
+{
+  Console.Write(output);
+  return Convert.ToInt32(Console.ReadLine());
+}
