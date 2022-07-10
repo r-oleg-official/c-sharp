98b090e30565dd794d47bbbf6791cc798d264ced
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-10 17:41:11 +0300

    Add "seminar-6/Example045_create_copy_array_Denis".

---
+++ seminar-6/Example045_create_copy_array_Denis/Program.cs
@@ -0,0 +1,42 @@
+﻿// Create copy a array.
+// Version by Denis.
+int size = InputInt("Введите размер массива: ");
+int[] numbers = new int[size];
+FillArray(numbers);
+int[] newNumbers = CopyArray(numbers);
+
+PrintArray(numbers);
+PrintArray(newNumbers);
+
+int[] CopyArray(int[] array)
+{
+  int[] copyArray = new int[array.Length];
+  for(int i = 0; i < copyArray.Length; i++)
+  {
+    copyArray[i] = array[i];
+  }
+  return copyArray;
+}
+
+int InputInt(string str)
+{
+  Console.Write(str);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int[] array)
+{
+  for(int i = 0; i< array.Length; i++)
+  {
+    array[i] = new Random().Next(1, 10);
+  }
+}
+
+void PrintArray(int[] array)
+{
+  for(int i = 0; i< array.Length; i++)
+  {
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
