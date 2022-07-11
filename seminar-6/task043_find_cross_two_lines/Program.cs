604c69fa0ed925d3b3b83d8147decb9423242788
r-oleg-official <r.oleg.official@gmail.com>
2022-07-11 10:42:53 +0300

    Add "seminar-6/task043_find_cross_two_lines".

---
+++ seminar-6/task043_find_cross_two_lines/Program.cs
@@ -0,0 +1,19 @@
+﻿// Have a two equations: y = k1 * x + b1, y = k2 * x + b2. User will be entering: b1, k1, b2 and k2.
+// You are need find a point of cross a two lines.
+// My version.
+
+double b1 = InputFactor("Enter b1: ");
+double k1 = InputFactor("Enter k1: ");
+double b2 = InputFactor("Enter b2: ");
+double k2 = InputFactor("Enter k2: ");
+
+double x = (b2-b1) / (k1 -k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($" -> ({x};{y}).");
+
+double InputFactor(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToDouble(Console.ReadLine());
+}
