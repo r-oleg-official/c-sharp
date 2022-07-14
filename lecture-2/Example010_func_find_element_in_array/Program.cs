3c4ad97b9b34c4458dd29ebcd1d69823ba990801
r-oleg-official <r.oleg.official@gmail.com>
2022-07-14 14:45:51 +0300

    Add lecture-2/Example010_func_find_element_in_array.

---
+++ lecture-2/Example010_func_find_element_in_array/Program.cs
@@ -0,0 +1,17 @@
+﻿// Find element into array.
+
+// indexes      0   1   2   3   4   5   6   7   8
+int[] array = {15, 21, 39, 11, 12, 18, 55, 78, 99};
+int n = array.Length;
+int find = 11;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+        {
+            Console.WriteLine(index);
+            break; // прервет цикл на 1-м срабатывании.
+        }
+    index++;
+}
