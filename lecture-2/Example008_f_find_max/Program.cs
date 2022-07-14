cecab95479a84cdb671268eb820c8b95922c9d37
r-oleg-official <r.oleg.official@gmail.com>
2022-07-14 12:07:48 +0300

    Edit "lecture-2/Example008_f_find_max".

--- lecture-2/Example008_f_find_max/Program.cs
+++ lecture-2/Example008_f_find_max/Program.cs
@@ -20,9 +20,16 @@ int a3 = 55;
 int b3 = 78;
 int c3 = 99;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// Version-1.
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+// Version-2. Относится к функциональномум программированию.
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
 
 Console.WriteLine(max);
