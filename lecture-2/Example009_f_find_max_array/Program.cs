950b271b8ca08440f6eb74853f559dc32773e23c
r-oleg-official <r.oleg.official@gmail.com>
2022-07-14 14:45:22 +0300

    Add lecture-2/Example009_f_find_max_array.

---
+++ lecture-2/Example009_f_find_max_array/Program.cs
@@ -0,0 +1,18 @@
+﻿// Find max number from a 9 numbers, using array.
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+// indexes    0   1   2   3   4   5   6   7   8
+int array = {15, 21, 39, 11, 12, 18, 55, 78, 99};
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
