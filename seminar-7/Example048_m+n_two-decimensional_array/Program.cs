9900a1da748ed4deba2883c9814cc7656df34046
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-16 21:44:05 +0300

    Add seminar-7/Example048_m+n_two-decimensional_array.

---
+++ seminar-7/Example048_m+n_two-decimensional_array/Program.cs
@@ -0,0 +1,38 @@
+﻿// Create a two-decimensional array 'm x n', where the every elements by Aₘₙ = m+n.
+// Output the array to screen.
+// My version.
+
+int m = InputInt("Введите количество строк: ");
+int n = InputInt("Введите количество столбцов: ");
+int[,] numbers = new int[m, n];
+FillArray(numbers);
+PrintArray(numbers);
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            numbers[i, j] = i + j;
+        }
+    }
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
