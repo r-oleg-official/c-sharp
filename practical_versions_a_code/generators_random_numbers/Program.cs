0edb40f57b3f1ca6d268abfa553dbe92fb0b3c26
r-oleg-official <r.oleg.official@gmail.com>
2022-07-18 12:59:07 +0300

    Add practical_versions_a_code/generators_random_numbers.

---
+++ practical_versions_a_code/generators_random_numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿// A different versions are a generators random numbers.
+
+Random random = new Random();
+
+int randomInteger = random.Next(0, 100);
+//                                          (max - min) + min
+double randomDouble = random.NextDouble() * (100 - 0) + 0;
+decimal randomDecimal = Convert.ToDecimal(random.NextDouble() * (100 - 0) + 0);
+float randomSingle = random.NextSingle() * (100 - 0) + 0;
+
+Console.WriteLine($"random = {random}");
+Console.WriteLine($"randomInteger = {randomInteger}");
+Console.WriteLine($"randomDouble = {randomDouble}");
+Console.WriteLine($"randomDecimal = {randomDecimal}");
+Console.WriteLine($"randomSingle = {randomSingle}");
