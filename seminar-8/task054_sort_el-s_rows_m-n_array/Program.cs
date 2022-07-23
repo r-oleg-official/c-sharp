e98771dddba0d55ca7e2ee2ca76415b0d407f659
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-23 18:04:13 +0300

    Well done "seminar-8/task054_sort_el-s_rows_m-n_array".

--- seminar-8/task054_sort_el-s_rows_m-n_array/Program.cs
+++ seminar-8/task054_sort_el-s_rows_m-n_array/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 // Create a two-demensional array. 
+// My version.
 
 int rows = GetInputValue("Введи количество строк: ");
 int columns = GetInputValue("Введи количество столбцов: ");
@@ -8,66 +9,27 @@ FillArray(matrix);
 PrintMatrix(matrix);
 Console.WriteLine();
 
-int[] varArray = new int[matrix.GetLength(1)];
-for (int i = 0; i < matrix.GetLength(1); i++)
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
-    varArray[i] = matrix[0, i];
-}
-
-
-for (int i = 0; i < varArray.GetLength(0); i++)
-{
-    Console.Write($"{varArray[i]} ");
-}
-Console.WriteLine();
-
-int max = 0;
-int tempVar = 0;
-// для рокировки элементов
-// int sizeRow = varArray.GetLength(0);
-int sizeRow = 0;
-while (sizeRow < varArray.GetLength(0))
-{
-    for (int j = 0; j < varArray.GetLength(0); j++)
+    for (int k = 0; k < matrix.GetLength(1) - 1; k++)
     {
-        if (max < varArray[j]) max = varArray[j];
+        bool stopSort = true;
+        int box = 0;
+        for (int j = 0; j < matrix.GetLength(1) - 1 - k; j++)
+        {
+            if (matrix[i,j] < matrix[i, j + 1])
+            {
+                box = matrix[i, j + 1];
+                matrix[i, j + 1] = matrix[i,j];
+                matrix[i,j] = box;
+                stopSort = false;
+            }
+        }
+        if (stopSort == true) break;
     }
-    tempVar = varArray[varArray.GetLength(0) - 1];
-    varArray[0] = max;
-    sizeRow++;
 }
 
-// for (int k = 0; k < sizeRow; k++)
-// {
-//     for (int j = 0; j < sizeRow; j++)
-//     {
-//         if (varArray[j] > varArray[j + 1])
-//         {
-//             tempVar = varArray[j];
-//             varArray[j] = varArray[j + 1];
-//             varArray[j + 1] = tempVar;
-//         }
-//     }
-//     sizeRow -= 1;
-// }
-
-
-
-for (int i = 0; i < varArray.GetLength(0); i++)
-{
-    Console.Write($"{varArray[i]} ");
-}
-Console.WriteLine();
-
-
-// SortElementsRows(matrix);
-// PrintMatrix(matrix);
-
-
-// double[] sumColumns = new double[matrix.GetLength(1)];
-// CalcAverangeColumns(matrix, sumColumns);
-// Console.Write("Среднее арифметическое каждого столбца: ");
-// PrintAverangeColumns(sumColumns);
+PrintMatrix(matrix);
 
 int GetInputValue(string msg)
 {
@@ -97,35 +59,3 @@ void PrintMatrix(int[,] array)
         Console.WriteLine();
     }
 }
-
-// void SortElementsRows(int[,] array)
-// {
-//     int min = 0; // для рокировки элементов
-//     int sizeRow = array.GetLength(1); // снижается после прохода массива, сдвиг макс вправо
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         sizeRow = array.GetLength(1);
-//         while (sizeRow < array.GetLength(1))
-//         {
-//             for (int j = 0; j < sizeRow; j++)
-//             {
-//                 if (array[i, j] > array[i + 1, j + 1])
-//                 {
-//                     min = array[i + 1, j + 1];
-//                     array[i + 1, j + 1] = array[i, j];
-//                     array[i, j] = min;
-//                     sizeRow++;
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// void PrintAverangeColumns(double[] array)
-// {
-//     for (int i = 0; i < array.Length - 1; i++)
-//     {
-//         Console.Write($"{array[i]}; ");
-//     }
-//     Console.WriteLine($"{array[array.Length - 1]}.");
-// }
