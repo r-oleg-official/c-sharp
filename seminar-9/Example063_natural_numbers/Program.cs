9b71389a11d893554b0ad5f743dd3992364954fa
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-23 12:14:39 +0300

    Add seminar-9/Example063_natural_numbers/.

---
+++ seminar-9/Example063_natural_numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter N: ");
+int number = Convert.ToInt32(ReadLine());
+Console.WriteLine(NaturalNumber(number));
+
+int NaturalNumber(int number);
+{
+    if(number == 1)
+        return 1;
+    else
+        Console.Write(NaturalNumber(number -1) + " ");
+        return number;
+}
