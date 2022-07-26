8ba033515cf65d649211e3dc43e94c996450397f
Oleg Razumov <r.oleg.official@gmail.com>
2022-07-26 09:37:07 +0300

    Add version by Denis "seminar-8/task062_fill_spiral_array".

---
+++ seminar-8/task062_fill_spiral_array/Program.cs
@@ -0,0 +1,30 @@
+﻿// Fill spiral array 4x4.
+// Version by Denis.
+int size = 4;
+int[,] array = new int[size, size];
+
+int value = 1;
+int i = 0;
+int j = 0;
+while(value <= size * size)
+{
+    array[i, j] = value;
+    if(i <= j + 1 && i + j < size - 1)
+        ++j;
+    else if (i < j && i + j >= size - 1)
+        ++i;
+    else if (i >= j && i + j > size - 1)
+        --j;
+    else
+        --i;
+    ++value;
+}
+
+for(int k = 0; k < array.GetLength(0); k++)
+{
+    for (int l = 0; l < array.GetLength(1); l++)
+    {
+        Console.Write(array[k, l] + " ");
+    }
+    Console.WriteLine();
+}
