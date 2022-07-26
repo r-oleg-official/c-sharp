62dc0984775a1e0a9b7c317797cdd9609106fde4
r-oleg-official <r.oleg.official@gmail.com>
2022-07-26 20:33:34 +0300

    Well done "seminar-8/task056_find_row_with_smallest_sum_m-n_array".

--- seminar-8/task056_find_row_with_smallest_sum_m-n_array/Program.cs
+++ seminar-8/task056_find_row_with_smallest_sum_m-n_array/Program.cs
@@ -5,13 +5,12 @@ Console.Clear();
 int rows = GetInputValue("Enter a number of rows: ");
 int columns = GetInputValue("Enter a number of columns: ");
 int[,] matrix = new int[rows, columns];
-// int[] summRows = new int[rows];
+int[] summRows = new int[rows];
+
 FillMatrix(matrix);
 PrintMatrix(matrix);
 Console.WriteLine();
-
-int min_row = CalcSumRow(matrix);
-Console.WriteLine($"Наименьшая сумма элементов в строке № {min_row}");
+CalcSumRowToArray(matrix, summRows);
 
 int GetInputValue(string msg)
 {
@@ -21,45 +20,49 @@ int GetInputValue(string msg)
 
 void FillMatrix(int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int row = 0; row < array.GetLength(0); row++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int col = 0; col < array.GetLength(1); col++)
         {
-            array[i, j] = Convert.ToInt32(new Random().Next(0, 10));
+            array[row, col] = Convert.ToInt32(new Random().Next(0, 10));
         }
     }
 }
 
 void PrintMatrix(int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int row = 0; row < array.GetLength(0); row++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int col = 0; col < array.GetLength(1); col++)
         {
-            Console.Write($"{array[i, j]} ");
+            Console.Write($"{array[row, col]} ");
         }
         Console.WriteLine();
     }
 }
 
-int CalcSumRow(int[,] sArray)
+void CalcSumRowToArray(int[,] sArray, int[] resArray)
 {
-    int min_index = 1;
-    int check = 0;
-    int sum = 0;
-    for (int i = 0; i < sArray.GetLength(0); i++)
+    for (int row = 0; row < sArray.GetLength(0); row++)
     {
-        // int check = 0;
-        sum = 0;
-        for (int j = 0; j < sArray.GetLength(1); j++)
+        int sum = 0;
+        for (int col = 0; col < sArray.GetLength(1); col++)
         {
-            sum += sArray[i, j];
+            sum = sum + sArray[row, col];
         }
-        if (sum < check)
-        {
-            check = sum;
-            min_index++;
-        }
+        resArray[row] = sum;
     }
-    return min_index;
+    int min = resArray[0];
+    int min_row = 1;
+    int i = 1;
+    while (i < resArray.Length)
+    {
+        if (resArray[i] < min)
+        {
+            min = resArray[i];
+            min_row = i + 1;
+        }
+        i++;
+    }
+    Console.WriteLine($"Наименьшая сумма элементов в строке № {min_row}");
 }
