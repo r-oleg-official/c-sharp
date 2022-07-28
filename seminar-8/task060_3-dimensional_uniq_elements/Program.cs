a6af4f7335ee66d5b10eacb253cf49cee3db8d2d
r-oleg-official <r.oleg.official@gmail.com>
2022-07-28 13:01:28 +0300

    Done PrintCube() in seminar-8/task060_3-dimensional_uniq_elements.

--- seminar-8/task060_3-dimensional_uniq_elements/Program.cs
+++ seminar-8/task060_3-dimensional_uniq_elements/Program.cs
@@ -1,15 +1,21 @@
-﻿
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// массив размером 2 x 2 x 1
+// My version.
+
 Console.Clear();
 int rows = GetInputValue("Enter a number rows of the cube: ");
 int cols = GetInputValue("Enter a number columns of the cube: ");
 int pages = GetInputValue("Enter a number pages of the cube: ");
 int[,,] cube = new int[rows, cols, pages];
 // int sizeUniqArray = cube.Length;
-int[] uniq = new int[cube.Length];
+// int[] uniq = new int[cube.Length];
 // Console.WriteLine(cube.Length);
 
-FillArrayUniqNumbers(uniq);
-PrintArray(uniq);
+FillCubeUniqNumbers(cube);
+PrintCube(cube);
+
+// PrintArray(uniq);
 
 
 
@@ -28,47 +34,64 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
+void PrintCube(int[,,] cube)
+{
+    for (int page = 0; page < cube.GetLength(2); page++)
+    {
+        Console.WriteLine($"Page - {page + 1}");
+        for (int row = 0; row < cube.GetLength(0); row++)
+        {
+            // Console.WriteLine("---------");
+            Console.Write("|");
+            for (int col = 0; col < cube.GetLength(1); col++)
+            {
+                // Console.Write($"| {row},{col},{page} |");
+                Console.Write($"| {cube[row, col, page]}({row},{col},{page}) |");
+            }
+            // Console.Write("----------------");
+            Console.WriteLine("|");
+            // Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
 // void FillArrayUniqNumbers()
 
-// // Standart version is filling the cube.
-// void FillCube(int[,,] cube)
+// Standart version is filling the cube.
+void FillCubeUniqNumbers(int[,,] cube)
+{
+    for (int row = 0; row < cube.GetLength(0); row++)
+    {
+        for (int col = 0; col < cube.GetLength(1); col++)
+        {
+            for (int page = 0; page < cube.GetLength(2); page++)
+            {
+                // for (int k = 0; k < cube.Get)
+                cube[row, col, page] = new Random().Next(1, 21);
+            }
+        }
+    }
+}
+
+// void FillArrayUniqNumbers(int[] array)
 // {
-//     for (int row = 0; row < cube.GetLength(0); row++)
-//         for (int col = 0; col < cube.GetLength(1); col++)
-//             for (int page = 0; page < cube.GetLength(2); page++)
-//             {
-//                 cube[row, col, page] = new Random.Next(1, 100);
-//             }
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         int element = new Random().Next(10, 100);
+//         if (FindElement(array, element) == false) array[i] = element;
+//     }
 // }
 
-void FillArrayUniqNumbers(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        int element = new Random().Next(10, 100);
-        if (FindElement(array, element) == false) array[i] = element;
-    }
-}
-
-bool FindElement(int[] uniq, int element)
-{
-    // int element = uniq[0];
-    bool check = false;
-    for (int i = 0; i < uniq.Length; i++)
-    {
-        if (element == uniq[i]) check = true;
-    }
-    return check;
-}
-
-
-// void FillCube(int[,,] cube, int[] uniq)
+// bool FindElement(int[] uniq, int element)
 // {
-//     int uniq = 0;
-//     for (int row = 0; row < cube.GetLength(0); row++)
-//         for (int col = 0; col < cube.GetLength(1); col++)
-//             for (int page = 0; page < cube.GetLength(2); page++)
-//             {
-//                 int
-//             }
+//     // int element = uniq[0];
+//     bool check = false;
+//     for (int i = 0; i < uniq.Length; i++)
+//     {
+//         if (element == uniq[i]) check = true;
+//     }
+//     return check;
 // }
