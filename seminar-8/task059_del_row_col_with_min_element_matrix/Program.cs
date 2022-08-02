b2dbea2f694a7e82499d143ca6ee8d364d82006b
r-oleg-official <r.oleg.official@gmail.com>
2022-08-02 17:21:46 +0300

    Add seminar-8/task059_del_row_col_with_min_element_matrix". By edition.

---
+++ seminar-8/task059_del_row_col_with_min_element_matrix/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Create a matrix and filling by the numbers, example, the random numbers.
+Delete are row and column on the cross with a minimal element to the matrix.
+My version 1.0.
+*/
+Console.Clear();
+int rows = GetInputValue("Enter a number of rows of the matrix: ");
+int columns = GetInputValue("Enter a number of columns of the matrix: ");
+// int minNumber = GetInputValue("Enter a minimal number to the matrix: ");
+// int maxNumber = GetInputValue("Enter a maximal number to the matrix: ");
+int[,] matrix = new int[rows, columns];
+int minNumber = 5;
+int maxNumber = 80;
+
+FillMatrix(matrix, minNumber, maxNumber);
+PrintMatrix(matrix);
+
+
+
+
+
+
+int GetInputValue(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillMatrix(int[,] array, int minValue, int MaxValue)
+{
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int col = 0; col < array.GetLength(1); col++)
+        {
+            array[row, col] = Convert.ToInt32(new Random().Next(minValue, MaxValue + 1));
+        }
+    }
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("| ");
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            Console.Write($"{array[i, j]}, ");
+        }
+        Console.WriteLine($"{array[i, array.GetLength(1) - 1]} |");
+    }
+}
