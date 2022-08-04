97026362a40427c390e014e2ea9cec9c486b743b
r-oleg-official <r.oleg.official@gmail.com>
2022-08-04 09:06:45 +0300

    Add "seminar-9/task064_natural_number_from_m_to_n".

---
+++ seminar-9/task064_natural_number_from_m_to_n/Program.cs
@@ -0,0 +1,26 @@
+﻿/* 
+Input M, N and displaying everything a natural numbers in range from M to N.
+My version.
+*/
+
+int m = GetInputValue("Enter a first number: ");
+int n = GetInputValue("Enter a second number: ");
+Console.Clear();
+Console.Write($"M = {m}; N = {n}. -> \"\"");
+Console.Write(NaturalNumber(m, n));
+Console.WriteLine("\"\"");
+Console.WriteLine();
+
+int GetInputValue(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NaturalNumber(int firstNumber, int secondNumber)
+{
+    if (secondNumber == firstNumber) return firstNumber;
+    else
+        Console.Write(NaturalNumber(firstNumber, secondNumber - 1) + ", ");
+    return secondNumber;
+}
