aaf4e38303fea3d1deec929d62e79c102249916a
Oleg Razumov <r.oleg.official@gmail.com>
2022-08-13 17:05:31 +0300

    Edit seminar-8/task060_3-dimensional_uniq_elements_v2.

--- seminar-8/task060_3-dimensional_uniq_elements_v2/Program.cs
+++ seminar-8/task060_3-dimensional_uniq_elements_v2/Program.cs
@@ -29,33 +29,33 @@ int GetInputValue(string msg)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-int GetRandomNumber(int minValue, int MaxValue)
-{
-    int random = new Random().Next(minValue, MaxValue + 1);
-    return random;
-}
+// int GetRandomNumber(int minValue, int MaxValue)
+// {
+//     int random = new Random().Next(minValue, MaxValue + 1);
+//     return random;
+// }
 
-void PrintCubeWithIndexes(int[,,] cube)
-{
-    for (int page = 0; page < cube.GetLength(2); page++)
-    {
-        Console.WriteLine($"Page - {page + 1}");
-        for (int row = 0; row < cube.GetLength(0); row++)
-        {
-            // Console.WriteLine("---------");
-            Console.Write("|");
-            for (int col = 0; col < cube.GetLength(1); col++)
-            {
-                // Console.Write($"| {row},{col},{page} |");
-                Console.Write($"| {cube[row, col, page]}({row},{col},{page}) |");
-            }
-            // Console.Write("----------------");
-            Console.WriteLine("|");
-            // Console.WriteLine();
-        }
-        Console.WriteLine();
-    }
-}
+// void PrintCubeWithIndexes(int[,,] cube)
+// {
+//     for (int page = 0; page < cube.GetLength(2); page++)
+//     {
+//         Console.WriteLine($"Page - {page + 1}");
+//         for (int row = 0; row < cube.GetLength(0); row++)
+//         {
+//             // Console.WriteLine("---------");
+//             Console.Write("|");
+//             for (int col = 0; col < cube.GetLength(1); col++)
+//             {
+//                 // Console.Write($"| {row},{col},{page} |");
+//                 Console.Write($"| {cube[row, col, page]}({row},{col},{page}) |");
+//             }
+//             // Console.Write("----------------");
+//             Console.WriteLine("|");
+//             // Console.WriteLine();
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
 void PrintCubeNotIndexes(int[,,] cube)
 {
@@ -119,32 +119,32 @@ bool FindElementCube(int[,,] cubik, int sElement)
 }
 
 // Sorting don't work.
-void SortCubeBubble(int[,,] cube)
-{
-    // int var = cube[0, 0, 0];
-    int var = 0;
-    bool isSort = false;
-    for (int k = 0; k < cube.Length; k++)
-    {
-        for (int page = 0; page < cube.GetLength(2); page++)
-        {
-            for (int row = 0; row < cube.GetLength(0); row++)
-            {
-                for (int col = 0; col < cube.GetLength(1) - 1; col++)
-                {
-                    if (cube[row, col, page] > cube[row, col + 1, page])
-                    {
-                        var = cube[row, col + 1, page];
-                        cube[row, col + 1, page] = cube[row, col, page];
-                        cube[row, col, page] = var;
-                    }
-                }
-            }
-            isSort = true;
-        }
-        if (isSort == false) break;
-    }
-}
+// void SortCubeBubble(int[,,] cube)
+// {
+//     // int var = cube[0, 0, 0];
+//     int var = 0;
+//     bool isSort = false;
+//     for (int k = 0; k < cube.Length; k++)
+//     {
+//         for (int page = 0; page < cube.GetLength(2); page++)
+//         {
+//             for (int row = 0; row < cube.GetLength(0); row++)
+//             {
+//                 for (int col = 0; col < cube.GetLength(1) - 1; col++)
+//                 {
+//                     if (cube[row, col, page] > cube[row, col + 1, page])
+//                     {
+//                         var = cube[row, col + 1, page];
+//                         cube[row, col + 1, page] = cube[row, col, page];
+//                         cube[row, col, page] = var;
+//                     }
+//                 }
+//             }
+//             isSort = true;
+//         }
+//         if (isSort == false) break;
+//     }
+// }
 
 
 // I'am try create a recursive function for filling the cube by unique numbers.
